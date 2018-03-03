ee3f46cc4e6a990634d3cab4d49d1ebfdb785d22
Simon Sapin <simon.sapin@exyr.org>
2018-03-03 15:01:50 +0100

    Keep generated files within $OUT_DIR by avoiding .. in source paths.

--- build.rs
+++ build.rs
@@ -15,28 +15,29 @@ fn main() {
 
     let data = if egl { build_data::EGL } else { build_data::TRANSLATOR };
 
-    // Change to one of the directory that contains moz.build
-    // (they’re on the same nesting level)
-    // so that paths like `../../checkout/src/…` line up.
     let repo = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
-    let translator = repo.join("gfx").join("angle").join("targets").join("translator");
-    env::set_current_dir(translator).unwrap();
+    env::set_current_dir(repo).unwrap();
 
+    // Change to one of the directory that contains moz.build
     let mut build = cc::Build::new();
-
-    for file in data.includes {
-        build.include(file);
-    }
 
     for &(k, v) in data.defines {
         build.define(k, v);
     }
 
+    for file in data.includes {
+        build.include(fixup_path(file));
+    }
+
+    for file in data.sources {
+        build.file(fixup_path(file));
+    }
+
     // Hard-code lines like `if CONFIG['OS_ARCH'] == 'Darwin':` in moz.build files
     for &(os, source) in &[
-        ("darwin", "../../checkout/src/common/system_utils_mac.cpp"),
-        ("linux", "../../checkout/src/common/system_utils_linux.cpp"),
-        ("windows", "../../checkout/src/common/system_utils_win.cpp"),
+        ("darwin", "gfx/angle/checkout/src/common/system_utils_mac.cpp"),
+        ("linux", "gfx/angle/checkout/src/common/system_utils_linux.cpp"),
+        ("windows", "gfx/angle/checkout/src/common/system_utils_win.cpp"),
     ] {
         if target.contains(os) {
             build.file(source);
@@ -45,15 +46,19 @@ fn main() {
     }
 
     build
-        .files(data.sources)
-        .file("../../../../src/shaders/glslang-c.cpp")
+        .file("src/shaders/glslang-c.cpp")
         .cpp(true)
         .warnings(false)
         .flag_if_supported("-msse2")  // GNU
         .flag_if_supported("-arch:SSE2")  // MSVC
         .compile("angle");
 
-    println!("cargo:rerun-if-changed=build_data.rs");
     println!("cargo:rerun-if-changed=src/shaders/glslang-c.cpp");
     println!("cargo:rerun-if-changed=gfx");
+}
+
+fn fixup_path(path: &str) -> String {
+    let prefix = "../../";
+    assert!(path.starts_with(prefix));
+    format!("gfx/angle/{}", &path[prefix.len()..])
 }
